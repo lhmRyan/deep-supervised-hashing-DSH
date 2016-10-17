53e1de5cd77b63b5fbf63a3a4947c4580b381ab8
lhmRyan <lhm199012@gmail.com>
2016-10-17 10:22:59 +0800

    Update solver
    Update solver for CIFAR-10 for faster training.

--- CIFAR-10/train_full.sh
+++ CIFAR-10/train_full.sh
@@ -3,5 +3,5 @@
 TOOLS=./build/tools
 
 $TOOLS/caffe train \
-    --solver=CIFAR-10/solver.prototxt  --gpu 3 #--weights=DNNH/cifar10_iter_50000.caffemodel #>>CIFAR-10/log.txt 2>&1 #\
+    --solver=CIFAR-10/solver.prototxt #--weights=DNNH/cifar10_iter_50000.caffemodel #>>CIFAR-10/log.txt 2>&1 #\
 #    --snapshot=CIFAR-10/cifar10_iter_30000.solverstate
